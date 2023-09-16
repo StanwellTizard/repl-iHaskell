844abddd60133b114a5304cb4da5fc574a21c00f
StanwellTizard <139468831+StanwellTizard@users.noreply.github.com>
2023-09-16 19:08:51 +0000

    Introduced distinction between Haskell Rational and rational numbers. Added tk package to enable dialog in FMGameIB

--- replit.nix
+++ replit.nix
@@ -1,1 +1,10 @@
-{ pkgs } : { deps = [ pkgs.haskellPackages.ihaskell pkgs.jupyter ]; }
+{ pkgs } : { deps = [ 
+                pkgs.haskellPackages.ihaskell
+                pkgs.jupyter
+                pkgs.python3
+                pkgs.python3Packages.pip
+                pkgs.python3Packages.pandas
+                pkgs.python3Packages.numpy
+                pkgs.python3Packages.tkinter
+    ];
+}
